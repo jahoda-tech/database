c0df613420c06175589d125b30a68d456324a745
Petr Jahoda <petrjahoda@protonmail.com>
2020-07-26 19:56:17 +0200

    Quick fix in go.mod
    Took 5 minutes

--- postgres/create.sh
+++ postgres/create.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 docker pull postgres:alpine
-docker rmi -f petrjahoda/database:$1
-docker build -t petrjahoda/database:$1 .
-docker push petrjahoda/database:$1
+docker rmi -f petrjahoda/database:latest
+docker build -t petrjahoda/database:latest .
+docker push petrjahoda/database:latest
+
+docker rmi -f petrjahoda/database:2020.3.1
+docker build -t petrjahoda/database:2020.3.1 .
+docker push petrjahoda/database:2020.3.1
