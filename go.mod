d08ae266b200daeaaad086bdd54b0bd7a11338ac
Petr Jahoda <petrjahoda@protonmail.com>
2020-08-22 12:26:57 +0200

    ## [2020.3.2.22] - 2020-08-22
    ### Added
    - automatic go get -u all when creating docker image
    
    Took 3 minutes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/petrjahoda/database
 
 go 1.13
 
-require gorm.io/gorm v0.2.24
+require gorm.io/gorm v0.2.35
--- postgres/create.sh
+++ postgres/create.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+cd ..
+go get -u all
+cd postgres
+
 docker pull postgres:alpine
 docker rmi -f petrjahoda/database:latest
 docker build -t petrjahoda/database:latest .
 docker push petrjahoda/database:latest
 
-docker rmi -f petrjahoda/database:2020.3.1
-docker build -t petrjahoda/database:2020.3.1 .
-docker push petrjahoda/database:2020.3.1
+docker rmi -f petrjahoda/database:2020.3.2
+docker build -t petrjahoda/database:2020.3.2 .
+docker push petrjahoda/database:2020.3.2
