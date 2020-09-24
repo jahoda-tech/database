df91d70892a099e42286037a1e6bc00275b1c599
Petr Jahoda <petrjahoda@protonmail.com>
2020-09-24 15:50:05 +0200

    ## [2020.3.3.24] - 2020-09-24
    ### Changed
    - docker image updated to postgres 13
    
    Took 4 minutes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/petrjahoda/database
 
 go 1.13
 
-require gorm.io/gorm v0.2.35
+require gorm.io/gorm v1.20.1
--- postgres/create.sh
+++ postgres/create.sh
@@ -3,11 +3,11 @@ cd ..
 go get -u all
 cd postgres
 
-docker pull postgres:alpine
+docker pull postgres:13-alpine
 docker rmi -f petrjahoda/database:latest
 docker build -t petrjahoda/database:latest .
 docker push petrjahoda/database:latest
 
-docker rmi -f petrjahoda/database:2020.3.2
-docker build -t petrjahoda/database:2020.3.2 .
-docker push petrjahoda/database:2020.3.2
+docker rmi -f petrjahoda/database:2020.3.3
+docker build -t petrjahoda/database:2020.3.3 .
+docker push petrjahoda/database:2020.3.3
