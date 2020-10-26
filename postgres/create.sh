bea06f4930723d3ac27589ecb4a167c8a0da6e72
Petr Jahoda <petrjahoda@protonmail.com>
2020-10-26 09:04:58 +0100

    ## [2020.4.1.26] - 2020-10-26
    ### Changed
    - updated to latest postgres
    
    Took 21 minutes

--- postgres/create.sh
+++ postgres/create.sh
@@ -8,6 +8,6 @@ docker rmi -f petrjahoda/database:latest
 docker build -t petrjahoda/database:latest .
 docker push petrjahoda/database:latest
 
-docker rmi -f petrjahoda/database:2020.3.3
-docker build -t petrjahoda/database:2020.3.3 .
-docker push petrjahoda/database:2020.3.3
+docker rmi -f petrjahoda/database:2020.4.1
+docker build -t petrjahoda/database:2020.4.1 .
+docker push petrjahoda/database:2020.4.1
