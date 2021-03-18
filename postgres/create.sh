ad8e6e82dd05605676e41ff5b3307d143d8fe77c
petrjahoda <petrjahoda@protonmail.com>
2021-03-18 10:10:45 +0100

    ## [2021.1.3.18] - 2021-03-18
    ### Changed
    - updated to latest postgres
    - updated to latest libraries
    
    Took 39 seconds

--- postgres/create.sh
+++ postgres/create.sh
@@ -12,6 +12,6 @@ docker rmi -f petrjahoda/database:latest
 docker build -t petrjahoda/database:latest .
 docker push petrjahoda/database:latest
 
-docker rmi -f petrjahoda/database:2021.1.2
-docker build -t petrjahoda/database:2021.1.2 .
-docker push petrjahoda/database:2021.1.2
+docker rmi -f petrjahoda/database:2021.1.3
+docker build -t petrjahoda/database:2021.1.3 .
+docker push petrjahoda/database:2021.1.3
