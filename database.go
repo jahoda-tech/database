6beab5c60af7fa8a0c3bf77a72c8cd15172bc47d
petrjahoda <petrjahoda@protonmail.com>
2021-06-23 13:59:56 +0200

    ## [2021.2.3.23] - 2021-06-23
    ### Changed
    
    - added again row for AlarmRecord and again for DowntimeRecord  and new for OrderRecord as sqlnull.Int32
    
    Took 42 seconds

--- database.go
+++ database.go
@@ -128,6 +128,7 @@ type OrderRecord struct {
 	OrderID         int `gorm:"uniqueIndex:unique_order_data"`
 	OperationID     int `gorm:"uniqueIndex:unique_order_data"`
 	WorkplaceID     int `gorm:"uniqueIndex:unique_order_data"`
+	UserId          sql.NullInt32
 	WorkplaceModeID int
 	WorkshiftID     int
 	AverageCycle    float32
